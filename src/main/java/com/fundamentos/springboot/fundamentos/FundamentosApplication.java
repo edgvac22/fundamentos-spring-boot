910fe4f9b07578eff13ab5a0a6f505570f6c86cc
edgvac2298 <edgardovac2298@gmail.com>
2022-01-06 15:37:17 -0500

    Create query method

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -55,13 +55,17 @@ public class FundamentosApplication implements CommandLineRunner {
 		LOGGER.info("Usuario con el metodo findByUserEmail"+ userRepository.findByUserEmail("edgardo@domain.com").orElseThrow(()->new RuntimeException("No se encontró el usuario")));
 
 		userRepository.findAndSort("user", Sort.by("id").descending()).stream().forEach(user -> LOGGER.info("Usuario con el metodo sort" +user));
+
+	userRepository.findByName("John").stream().forEach(user -> LOGGER.info("usuario con query method"+user.toString()));
+
+	LOGGER.info("usuario con query method findByEmailAndName"+ userRepository.findByEmailAndName("daniela@domain.com", "Daniela").orElseThrow(() -> new RuntimeException("usuario no encontrado")));
 	}
 
 	private void saveUsersInDatabase(){
 		User user1 = new User("John", "john@domain.com", LocalDate.of(2021, 03, 20 ));
-		User user2 = new User("Edgardo", "edgardo@domain.com", LocalDate.of(2021, 04, 25 ));
-		User user3 = new User("user", "juan@domain.com", LocalDate.of(2021, 05, 23 ));
-		User user4 = new User("user1", "daniela@domain.com", LocalDate.of(2021, 04, 17 ));
+		User user2 = new User("John", "edgardo@domain.com", LocalDate.of(2021, 04, 25 ));
+		User user3 = new User("Daniela", "daniela@domain.com", LocalDate.of(2021, 05, 23 ));
+		User user4 = new User("user1", "user1@domain.com", LocalDate.of(2021, 04, 17 ));
 		User user5 = new User("user2", "andrea@domain.com", LocalDate.of(2022, 01, 02 ));
 		User user6 = new User("user3", "maria@domain.com", LocalDate.of(2021, 06, 15 ));
 		User user7 = new User("user4", "diana@domain.com", LocalDate.of(2021, 07, 10 ));
